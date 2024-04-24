28db78a06f65154a25328cec12684e6f337aca4f
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-04-24 13:40:20 +0000

    Updated cask systemd-status-flutter to v0.1.7

--- Casks/systemd-status-flutter.rb
+++ Casks/systemd-status-flutter.rb
@@ -1,6 +1,6 @@
 cask "systemd-status-flutter" do
-  version "0.1.6"
-  sha256 "c5ed4fc00aa1fe493f6ce063d5f07ce4956b72358d199ba92debe0fb5ba2bb6a"
+  version "0.1.7"
+  sha256 "ae133a9604dda016ce3bbdecc19a036a3037d1df49f745db643d9b37361c4247"
 
   url "https://github.com/Skycoder42/systemd_status/releases/download/app%2Fv#{version}/Systemd.Status.dmg"
   name "Systemd Status"
