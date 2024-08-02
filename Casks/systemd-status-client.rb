93b39ad36f9691c5958403b573d27a13e98c8f49
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-08-02 07:00:44 +0000

    Updated cask systemd-status-client to v0.2.9

--- Casks/systemd-status-client.rb
+++ Casks/systemd-status-client.rb
@@ -1,6 +1,6 @@
 cask "systemd-status-client" do
-  version "0.2.8"
-  sha256 "ca6ecf727a6485e0870cba384381864e179c298248060bb31fba81c2cb430dc2"
+  version "0.2.9"
+  sha256 "9ab5fcb150e64446ccf5b9a284ae59eae5f41ad4fff75861745b5eec8c8f61bf"
 
   url "https://github.com/Skycoder42/systemd_status/releases/download/client%2Fv#{version}/Systemd.Status.dmg"
   name "Systemd Status"
